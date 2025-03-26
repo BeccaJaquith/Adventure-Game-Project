96f7817c508e8ca90c347c355da12ae84efaae59
Rebecca Jaquith <becca.jaquith@umconnect.umt.edu>
2025-03-25 21:47:17 -0600

    Assignment 9 - Game Loops Revision (game.py)

--- game.py
+++ game.py
@@ -9,14 +9,6 @@ The functions, along with the game.py file will allow the user to play a very ba
 import gamefunctions
 import random
 
-#Game variables.
-player_HP = 30
-player_gold = 10
-player_damage = random.randint(5, 10) #The damage the player does.
-monster_HP = random.randint(5, 50)
-monster_damage = random.randint(5, 10) #The damage the monster does.
-
-
 '''Call the main_loop_game to view the town menu and play the game.'''
 def main_game_loop():
     '''This function allows the user to play the game.
@@ -28,28 +20,25 @@ def main_game_loop():
     None.
 
     '''
-    
+    #Game variables.
+    player_HP = 30
+    player_gold = 10
+    player_damage = random.randint(5, 10) #The damage the player does.
+
+
     while True:
 
-        gamefunctions.print_town_menu()
-        
+        gamefunctions.print_town_menu(player_HP, player_gold)
         user_choice = gamefunctions.user_selection()
-
         if user_choice == 1: #Leave town and fight monster.
-            
-            gamefunctions.fight_monster()
-
+            player_HP, player_gold = gamefunctions.fight_monster(player_HP, player_damage, player_gold)
         elif user_choice == 2: #Sleep and restore HP.
-
-            gamefunctions.sleep()
-
+            player_HP, player_gold = gamefunctions.sleep(player_gold, player_HP)
         elif user_choice == 3: #Quit the game.
-
             print()
             print('Thank you for playing the game!')
             print()
             break
-
 
 if __name__ == "__main__":
     main_game_loop()
